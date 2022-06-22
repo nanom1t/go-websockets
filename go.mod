788982cfbeacd3a7c0d4593da8c928ee1cccc135
nanom1t <bitfire777@gmail.com>
2022-06-22 15:00:20 +0300

    Created websocket server

--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module github.com/nanom1t/go-websockets
 
 go 1.16
+
+require github.com/gorilla/websocket v1.5.0
--- main.go
+++ main.go
@@ -1,7 +1,62 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"net/http"
+	"time"
+	"log"
+	"github.com/gorilla/websocket"
+	"os"
+)
+
+var upgrader = websocket.Upgrader{}
+
+func websocketHandler(w http.ResponseWriter, r *http.Request) {
+	connection, err := upgrader.Upgrade(w, r, nil)
+	if err != nil {
+		log.Print("upgrade:", err)
+		return
+	}
+	defer connection.Close()
+
+	for {
+		messageType, message, err := connection.ReadMessage()
+		if err != nil {
+			log.Println("ERR:", err)
+			break
+		}
+
+		log.Printf("MESSAGE: %s", message)
+
+		err = connection.WriteMessage(messageType, message)
+		if err != nil {
+			log.Println("ERR:", err)
+			break
+		}
+	}
+}
+
+func indexHandler(w http.ResponseWriter, r *http.Request) {
+	w.Write([]byte("OK"))
+}
 
 func main() {
-	fmt.Println("Go Websockets")
+	host := os.Getenv("HOST")
+	port := os.Getenv("PORT")
+
+	server := &http.Server{
+		Addr:           fmt.Sprintf("%s:%s", host, port),
+		ReadTimeout:    10 * time.Second,
+		WriteTimeout:   10 * time.Second,
+	}
+
+	http.HandleFunc("/", indexHandler)
+	http.HandleFunc("/ws", websocketHandler)
+
+	log.Printf("Starting server on %s:%s", host, port)
+
+	err := server.ListenAndServe()
+	if err != nil {
+		log.Fatal(err)
+	}
 }
